dca91734f24b9cbebbc5c65c8ac345a43e73b4b9
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-03 07:20:08 +0000

    Completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+var sum = addTwoNumbers(2, 2);
+console.log(sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var hours = convertHoursToMinutes(2);
+console.log(hours);
+
+function getGreeting(name) {
+  return name + (' World!');
+}
+var name = getGreeting('Hello');
+console.log(name);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+var product = addAndMultiplyBy5(10, 5);
+console.log(product);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+var quotient = multiplyAndDivideBy5(35, 10);
+console.log(quotient);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var difference = subtractTwoNumbers(22, 7);
+console.log(difference);
+
+function getCircleCircumference(radius) {
+  return (radius * 2) * Math.PI;
+}
+var circumference = getCircleCircumference(5);
+console.log(circumference);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var fullName = getFullName('Juan', 'Ramirez');
+console.log(fullName);
+
+function cube(number) {
+  return number * number * number;
+}
+var solution = cube(5);
+console.log(solution);
