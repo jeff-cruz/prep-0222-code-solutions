c658f1d2287ee8982a2c0087d1ddfb94ad0aa7e5
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-03 20:11:38 +0000

    Completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var i;
+for (i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (i = 100; i > 0; i -= 1) {
+  console.log('Time till explosion: ' + i);
+}
